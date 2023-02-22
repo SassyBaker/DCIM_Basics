1ea387d2410289c5d2b8da60d78f9f82cbc433b8
Dev <georgioji@gmail.com>
2023-02-22 11:09:10 -0700

    Setup URLs

--- DCIM_Basics/urls.py
+++ DCIM_Basics/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rack/', include('Racks.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
---
+++ Racks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from .views import VIEW
+
+urlpatterns = [
+    # path('NAME', 'VIEW.as_view')
+]
